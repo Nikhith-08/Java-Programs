3d993c6ac66cfb389084914420f5f4387bb89562
Bochkar Nikhith <115868325+Nikhith-08@users.noreply.github.com>
2024-03-06 00:16:45 +0530

    Update DispEmpDetails.java

--- DispEmpDetails.java
+++ DispEmpDetails.java
@@ -9,8 +9,9 @@ class Employee{
 class DispEmpDetails {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        System.out.print("Enter how many employees: ");
+        System.out.print("\nEnter how many employees: ");
         int n = s.nextInt();
+        System.out.println();
         Employee emp[] = new Employee[n];
         for(int i=0; i<n; i++){
             emp[i] = new Employee();
@@ -26,7 +27,7 @@ class DispEmpDetails {
             System.out.println();
         }
         for(int i=0; i<n; i++){
-            System.out.println("Employee "+(i+1)+" details");
+            System.out.println("Employee "+(i+1)+" details:");
             System.out.println("Employee id: "+emp[i].id);
             System.out.println("Employee name: "+emp[i].name);
             System.out.println("Employee designation: "+emp[i].designation);
