ce0c8361435f8c54af529e9ba18ce9562e08dde4
Bochkar Nikhith <115868325+Nikhith-08@users.noreply.github.com>
2024-06-17 18:37:43 +0530

    Update MultiThread.java

--- new/MultiThread.java
+++ new/MultiThread.java
@@ -12,11 +12,6 @@ class MyRunnable implements Runnable {
     public void run() {
         for (int i = 0; i < 4; i++) {
             System.out.println(Thread.currentThread().getName() + ": " + i);
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
         }
     }
 }
